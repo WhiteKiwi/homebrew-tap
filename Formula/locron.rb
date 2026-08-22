ecdd691ea7f0be313fd6cb762306ee023473eb20
whitekiwi <jh145478@gmail.com>
2026-08-23 01:26:58 +0900

    bump(locron): 0.1.0

--- Formula/locron.rb
+++ Formula/locron.rb
@@ -7,20 +7,20 @@ class Locron < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/WhiteKiwi/locron/releases/download/v#{version}/locron-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_AARCH64_APPLE_DARWIN"
+      sha256 "ddc3c79969161ee41ceec89a40da522e92aa6b96f3954373699ab1b3c65acfd4"
     else
       url "https://github.com/WhiteKiwi/locron/releases/download/v#{version}/locron-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_X86_64_APPLE_DARWIN"
+      sha256 "308b1f8de316138bcf0a575d021ce77a2a81bd584c9d6aef9f7a4d96af043d51"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/WhiteKiwi/locron/releases/download/v#{version}/locron-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_AARCH64_UNKNOWN_LINUX_GNU"
+      sha256 "5d6607feb06b6c7dad364cc5208820f744c2206c7b7d70ce1499ef7d0454fb30"
     else
       url "https://github.com/WhiteKiwi/locron/releases/download/v#{version}/locron-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_X86_64_UNKNOWN_LINUX_GNU"
+      sha256 "d323f9fa24450fe555a1f6021f973671331d3c460a8dbdd397cc386dd77c6019"
     end
   end
 
